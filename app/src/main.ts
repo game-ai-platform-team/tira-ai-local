71d06ab345d108936794d78c8edf9ee35a6884c5
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-03-08 17:43:42 +0200

    feat(#1): Start working on python integration

--- app/src/main.ts
+++ app/src/main.ts
@@ -14,4 +14,16 @@ app.on("ready", () => {
 
     win.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
     win.webContents.openDevTools()
+
+    console.log("Spawning process")
+    const spawned = spawn("poetry", ["run", "python3", "./src/main.py"]);
+    spawned.on("message", (message) => {
+        console.log(message)
+    })
+    spawned.on("error", (message) => {
+        console.log(message)
+    })
+    spawned.on("exit", (message) => {
+        console.log("exit")
+    })
 })
