62d39d7c82b99deae89582feac16a4c9e20f4ec6
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-03-13 15:29:01 +0200

    feat(#1): Hide the board before submission

--- app/src/renderer/components/AIForm.tsx
+++ app/src/renderer/components/AIForm.tsx
@@ -1,31 +1,31 @@
-import { useState } from "react";
+import {useState} from "react";
 
 function AIForm(props) {
-  const [filepath, setFilePath] = useState("/home/miika_piiparinen/tira-project/stupid-chess-ai");
-  const [fennotation, setFenNotation] = useState("8/8/8/2k5/4K3/8/8/8 w - - 4 45");
+    const [filepath, setFilePath] = useState("");
+    const [fennotation, setFenNotation] = useState("");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    props.handleSubmit(filepath, fennotation);
-  }
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.handleSubmit(filepath, fennotation);
+    }
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <button type="submit">SUBMIT</button>
-        File path: <input
-          id="fileinput"
-          value={filepath}
-          onChange={(e) => setFilePath(e.target.value)}
-        />
-        Fen board: <input
-          id="feninput"
-          value={fennotation}
-          onChange={(e) => setFenNotation(e.target.value)}
-        /> (optional)
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <button type="submit">SUBMIT</button>
+                File path: <input
+                id="fileinput"
+                value={filepath}
+                onChange={(e) => setFilePath(e.target.value)}
+            />
+                Fen board: <input
+                id="feninput"
+                value={fennotation}
+                onChange={(e) => setFenNotation(e.target.value)}
+            /> (optional)
+            </form>
+        </div>
+    );
 }
 
 export default AIForm;
--- app/src/renderer/components/MainView.tsx
+++ app/src/renderer/components/MainView.tsx
@@ -1,24 +1,27 @@
 import AIForm from "./AIForm";
-import { MyChessboard } from "./MyChessboard";
-import { Logs } from "./Logs";
+import {MyChessboard} from "./MyChessboard";
+import {Logs} from "./Logs";
 import socket from "../MySocket";
-import { useState } from "react";
-import { Position, } from "kokopu";
+import {useState} from "react";
+import {Position,} from "kokopu";
 
 export function MainView() {
     const [pos, setPos] = useState(new Position());
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false)
 
     function handleSubmit(filepath, fennotation) {
         socket.emit("startgame", filepath, fennotation);
+        setHasBeenSubmitted(true)
         try {
             setPos(new Position(fennotation));
-        } catch (error) {}
+        } catch (error) {
+        }
     }
 
-    return(
-    <div>
-        <MyChessboard pos={pos} setPos={setPos} />
-        <Logs/>
-        <AIForm handleSubmit={handleSubmit}/>
-    </div>)
+    return (
+        <div>
+            {hasBeenSubmitted && <MyChessboard pos={pos} setPos={setPos}/>}
+            <Logs/>
+            <AIForm handleSubmit={handleSubmit}/>
+        </div>)
 }
