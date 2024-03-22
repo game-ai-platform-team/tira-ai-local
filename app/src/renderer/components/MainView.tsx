c6f91ee62056a0f4c5176c708c6e4c542301a936
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-03-22 12:50:39 +0200

    feat(#3): Add initial check for game over
    Co-authored-by: Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>

--- app/src/renderer/components/MainView.tsx
+++ app/src/renderer/components/MainView.tsx
@@ -1,43 +1,46 @@
 import AIForm from "./AIForm";
-import {MyChessboard} from "./MyChessboard";
-import {Logs} from "./Logs";
+import { MyChessboard } from "./MyChessboard";
+import { Logs } from "./Logs";
 import socket from "../MySocket";
-import {useState} from "react";
-import {Position} from "kokopu";
+import { useState } from "react";
+import { Position } from "kokopu";
 
 export function MainView() {
-    const [pos, setPos] = useState(new Position());
-    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+  const [pos, setPos] = useState(new Position());
+  const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
 
-    function handleSubmit(filepath: string, fennotation: string, runsetup: boolean) {
-        socket.emit("startgame", filepath, fennotation, runsetup);
-        setHasBeenSubmitted(true);
-        try {
-            setPos(new Position(fennotation));
-        } catch (error) {
-            setPos(new Position());
-        }
+  function handleSubmit(
+    filepath: string,
+    fennotation: string,
+    runsetup: boolean
+  ) {
+    socket.emit("startgame", filepath, fennotation, runsetup);
+    setHasBeenSubmitted(true);
+    try {
+      setPos(new Position(fennotation));
+    } catch (error) {
+      setPos(new Position());
     }
+  }
 
-    function copyFenToClipboard() {
-        const fen = pos.fen();
-        navigator.clipboard.writeText(fen)
-            .then(() => {
-                console.log('Copied FEN to clipboard:', fen);
-            })
-            .catch(err => {
-                console.error('Error copying FEN notation to clipboard:', err);
-            });
-    }
+  function copyFenToClipboard() {
+    const fen = pos.fen();
+    navigator.clipboard
+      .writeText(fen)
+      .then(() => {
+        console.log("Copied FEN to clipboard:", fen);
+      })
+      .catch((err) => {
+        console.error("Error copying FEN notation to clipboard:", err);
+      });
+  }
 
-    return (
-        <div>
-            {hasBeenSubmitted && <MyChessboard pos={pos} setPos={setPos}/>}
-            <Logs/>
-            <button onClick={copyFenToClipboard}>
-                Copy current FEN
-            </button>
-            <AIForm handleSubmit={handleSubmit}/>
-        </div>
-    );
+  return (
+    <div>
+      {hasBeenSubmitted && <MyChessboard pos={pos} setPos={setPos} />}
+      <Logs />
+      <button onClick={copyFenToClipboard}>Copy current FEN</button>
+      <AIForm handleSubmit={handleSubmit} />
+    </div>
+  );
 }
--- app/src/renderer/components/MyChessboard.tsx
+++ app/src/renderer/components/MyChessboard.tsx
@@ -1,37 +1,46 @@
 import { Chessboard } from "kokopu-react";
 import { useState } from "react";
-import { Position } from "kokopu";
+import { Position, Game } from "kokopu";
 import socket, { sethandleMovePlayedByAi } from "../MySocket";
 
+export function MyChessboard(props: {
+  pos: Position;
+  setPos: (Position) => void;
+}) {
+  function handleMovePlayed(move: string) {
+    props.setPos((prevState) => {
+      const copy = new Position(prevState);
+      const move_desc = copy.notation(move);
+      socket.emit("move_to_back", copy.uci(move_desc));
+      copy.play(move_desc);
+      return copy;
+    });
+  }
+  function handleMovePlayedByAi(move: string) {
+    props.setPos((prevState) => {
+      const copy = new Position(prevState);
+      copy.play(copy.uci(move));
+      return copy;
+    });
+  }
 
-export function MyChessboard(props) {
+  if (
+    props.pos.isCheckmate() === true ||
+    props.pos.isStalemate() === true ||
+    props.pos.isDead() === true
+  ) {
+    console.log("game over");
+  }
 
-    function handleMovePlayed(move: string) {
-        props.setPos((prevState) => {
-            const copy = new Position(prevState);
-            const move_desc = copy.notation(move)
-            socket.emit("move_to_back", copy.uci(move_desc));
-            copy.play(move_desc);
-            return copy;
-        });
-    }
-    function handleMovePlayedByAi(move: string) {
-        props.setPos((prevState) => {
-            const copy = new Position(prevState);
-            copy.play(copy.uci(move));
-            return copy;
-        });
-    }
+  sethandleMovePlayedByAi(handleMovePlayedByAi);
 
-    sethandleMovePlayedByAi(handleMovePlayedByAi)
-
-    return (
-        <div>
-            <Chessboard
-                interactionMode={"playMoves"}
-                onMovePlayed={(move) => handleMovePlayed(move)}
-                position={props.pos}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Chessboard
+        interactionMode={"playMoves"}
+        onMovePlayed={(move) => handleMovePlayed(move)}
+        position={props.pos}
+      />
+    </div>
+  );
 }
