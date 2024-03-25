cf1c18d5c42c0938a6f64ecee3226adbf9c60aab
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-03-25 15:01:08 +0200

    test(#1): Add test

--- background-service/src/tests/entities/test_game.py
+++ background-service/src/tests/entities/test_game.py
@@ -1,5 +1,14 @@
 from unittest import TestCase
+from unittest.mock import Mock
+
+from entities.game import Game
+
 
 class TestGame(TestCase):
     def setUp(self) -> None:
         pass
+
+    def test_game_calls_ai_directory(self):
+        mock = Mock()
+        game = Game(mock)
+        mock.run_ai.assert_called()
