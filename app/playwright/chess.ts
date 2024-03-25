665f5c578526a4fa66743082cc293e36fc4698d5
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-03-25 14:34:17 +0200

    test: Make e2e test work

--- app/playwright/chess.ts
+++ app/playwright/chess.ts
@@ -1,8 +1,13 @@
-import { test, _electron as electron } from 'playwright/test'
+import {test, expect, _electron as electron} from 'playwright/test'
 
 test('launch app', async () => {
-  const electronApp = await electron.launch()
-  // close app
-  await electronApp.close()
+    const electronApp = await electron.launch({
+        args: ["./out/tira-ai-local-linux-x64/resources/app/.webpack/main/index.js"],
+        timeout: 60000
+    })
+    const window = await electronApp.firstWindow({timeout: 60000});
+    await expect(window.locator("#submit")).toBeVisible();
+    // close app
+    await electronApp.close()
 })
 
