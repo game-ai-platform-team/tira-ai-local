37d3a0b33c8bd33fffb58bd85cd8475c2c3d06ff
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-03-26 15:20:04 +0200

    test(#1): Add tests for game service, move to back
    Co-authored-by: mleikas@helsinki.fi <max.o.leikas@helsinki.fi>

--- background-service/src/tests/services/test_game_service.py
+++ background-service/src/tests/services/test_game_service.py
@@ -26,3 +26,36 @@ class TestGameService(TestCase):
 
         log = f"Error creating game:\n{str(exception)}"
         socket_service.send_log.assert_called_with(log)
+
+    def test_move_to_back_works(self):
+        ai_move = "a7a5"
+        player_move = "a2a3"
+
+        ai_directory = Mock()
+        ai_directory.play.return_value = ai_move, []
+        socket_service = Mock()
+        game_service = GameService(socket_service)
+        game_service.start_game(ai_directory, "", False)
+
+        game_service.move_to_back(player_move)
+
+        ai_directory.move.assert_called_once_with(player_move)
+        socket_service.move_to_front.assert_called_once_with(ai_move)
+
+    def test_move_to_back_fails(self):
+        ai_move = "a7a5"
+        player_move = "a2a3"
+
+        ai_directory = Mock()
+
+        exception = RuntimeError("Fail :--DDDD")
+        ai_directory.play = Mock(side_effect = exception)
+
+        socket_service = Mock()
+        game_service = GameService(socket_service)
+        game_service.start_game(ai_directory, "", False)
+
+        game_service.move_to_back(player_move)
+
+        log = str(exception) + "\n---------------------------------"
+        socket_service.send_log.assert_called_with(log)
