6bcd640a78fea7d06e0461cc3e01bd041a655ed7
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-03-29 10:46:44 +0200

    Feat(#4): Hide CFour when nothing has been submitted

--- app/src/renderer/components/cfour/CFourView.tsx
+++ app/src/renderer/components/cfour/CFourView.tsx
@@ -7,66 +7,71 @@ import checkForWin from "./CFourWinChecker";
 import CFourParser from "./CFourParser";
 
 export function CFourView() {
-    const [boardIndex, setBoardIndex] = useState(-1);
-    const [moves, setMoves] = useState([]);
-    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+  const [boardIndex, setBoardIndex] = useState(-1);
+  const [moves, setMoves] = useState([]);
+  const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
 
-    function handleSubmit(
-        filepath: string,
-        fennotation: string,
-        runsetup: boolean
-    ) {
-        socket.emit("startgame", filepath, fennotation, runsetup);
-        setBoardIndex(0);
-        setMoves([]);
-        setHasBeenSubmitted(true);
+  function handleSubmit(
+    filepath: string,
+    fennotation: string,
+    runsetup: boolean
+  ) {
+    socket.emit("startgame", filepath, fennotation, runsetup);
+    setBoardIndex(0);
+    setMoves([]);
+    setHasBeenSubmitted(true);
+  }
+
+  function handleMovePlayedByAi(move: string) {
+    if (!isGameOver) {
+      moves.push(parseInt(move));
+      setBoardIndex(boardIndex + 1);
     }
+  }
 
-    function handleMovePlayedByAi(move: string) {
-        if (!isGameOver) {
-            moves.push(parseInt(move));
-            setBoardIndex(boardIndex + 1);
-        }
+  function handleMovePlayed(move: string) {
+    socket.emit("move_to_back", move);
+    moves.push(move);
+    setBoardIndex(boardIndex + 1);
+  }
+
+  sethandleMovePlayedByAi(handleMovePlayedByAi);
+
+  function handlePrevMoveButton() {
+    if (boardIndex > 0) {
+      setBoardIndex(boardIndex - 1);
     }
+  }
 
-    function handleMovePlayed(move: string) {
-        socket.emit("move_to_back", move);
-        moves.push(move);
-        setBoardIndex(boardIndex + 1);
+  function handleNextMoveButton() {
+    if (boardIndex < moves.length - 1) {
+      setBoardIndex(boardIndex + 1);
     }
+  }
 
-    sethandleMovePlayedByAi(handleMovePlayedByAi);
+  const boardmatrix = CFourParser({ moves, boardIndex });
+  const isGameOver = checkForWin(boardmatrix) || moves.length >= 42;
 
-    function handlePrevMoveButton() {
-        if (boardIndex > 0) {
-            setBoardIndex(boardIndex - 1);
-        }
-    }
+  return (
+    <div>
+      {hasBeenSubmitted && (
+        <>
+          <MyConnectFour
+            moves={moves}
+            boardIndex={boardIndex}
+            onMovePlayed={handleMovePlayed}
+            active={!isGameOver}
+          />
+          <div>
+            <button onClick={handlePrevMoveButton}>{"<"}</button>
+            <button onClick={handleNextMoveButton}>{">"}</button>
+          </div>
 
-    function handleNextMoveButton() {
-        if (boardIndex < moves.length - 1) {
-            setBoardIndex(boardIndex + 1);
-        }
-    }
-
-    const boardmatrix = CFourParser({ moves, boardIndex });
-    const isGameOver = checkForWin(boardmatrix) || moves.length >= 42;
-
-    return (
-        <div>
-            <MyConnectFour
-                moves={moves}
-                boardIndex={boardIndex}
-                onMovePlayed={handleMovePlayed}
-                active={!isGameOver}
-            />
-            <div>
-                <button onClick={handlePrevMoveButton}>{"<"}</button>
-                <button onClick={handleNextMoveButton}>{">"}</button>
-            </div>
-            {isGameOver && <div>GAME OVER</div>}
-            <Logs />
-            <AIForm handleSubmit={handleSubmit} showFen={false} />
-        </div>
-    );
+          {isGameOver && <div>GAME OVER</div>}
+        </>
+      )}
+      <Logs />
+      <AIForm handleSubmit={handleSubmit} showFen={false} />
+    </div>
+  );
 }
