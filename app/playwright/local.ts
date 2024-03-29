40c447901ba4bcac536c3f248a2750bd81b42957
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-03-29 13:07:54 +0200

    e2e test for connect four

--- app/playwright/local.ts
+++ app/playwright/local.ts
@@ -1,6 +1,6 @@
 import {test, expect, _electron as electron} from 'playwright/test'
 
-test('field form', async () => {
+test('Chess field form', async () => {
     const electronApp = await electron.launch({
         args: ["./out/tira-ai-local-linux-x64/resources/app/.webpack/main/index.js"],
         timeout: 60000
@@ -12,6 +12,19 @@ test('field form', async () => {
   await window.locator("#submit").click()
   await electronApp.close()
 })
+
+test('Connect Four field form', async () => {
+    const electronApp = await electron.launch({
+        args: ["./out/tira-ai-local-linux-x64/resources/app/.webpack/main/index.js"],
+        timeout: 60000
+    })
+  const window = await electronApp.firstWindow();
+  window.getByText("Connect Four").click()
+  await window.locator("#fileinput").fill("/")
+  await window.locator("#submit").click()
+  await electronApp.close()
+})
+
 test('launch app', async () => {
     const electronApp = await electron.launch({
         args: ["./out/tira-ai-local-linux-x64/resources/app/.webpack/main/index.js"],
