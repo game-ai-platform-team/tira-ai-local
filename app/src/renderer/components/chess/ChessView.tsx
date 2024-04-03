9f7233c00a81787809e37d2eeff3f25685074f3a
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-04-03 14:40:15 +0300

    feat(#7): Copy PGN button
    Co-authored-by: Joel Weinitschke <joel.weinitschke@gmail.com>

--- app/src/renderer/components/chess/ChessView.tsx
+++ app/src/renderer/components/chess/ChessView.tsx
@@ -8,137 +8,144 @@ import { createPGNString } from "../../PGNFormatter";
 import { sendBoardFen } from "../../MoveSender";
 
 export function ChessView() {
-    const [positions, setPositions] = useState([new Position()]);
-    const [boardIndex, setBoardIndex] = useState(0);
-    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+  const [positions, setPositions] = useState([new Position()]);
+  const [boardIndex, setBoardIndex] = useState(0);
+  const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
 
-    const [moves, setMoves] = useState([]);
-    const [halfMoves, setHalfMoves] = useState([]);
+  const [moves, setMoves] = useState([]);
+  const [halfMoves, setHalfMoves] = useState([]);
 
-    const fullMoves = (index: number) => {
-        return Math.floor(index / 2) + 1;
-    };
+  const fullMoves = (index: number) => {
+    return Math.floor(index / 2) + 1;
+  };
 
-    function handleSubmit(
-        filepath: string,
-        fennotation: string,
-        runsetup: boolean
-    ) {
-        socket.emit("startgame", filepath, fennotation, runsetup);
-        setBoardIndex(0);
-        setHasBeenSubmitted(true);
-		const fenList = fennotation.split(" ")
-		setHalfMoves([parseInt(fenList[4])])
-        setMoves([]);
-        try {
-            setPositions([new Position(fennotation)]);
-        } catch (error) {
-            setPositions([new Position()]);
-        }
+  function handleSubmit(
+    filepath: string,
+    fennotation: string,
+    runsetup: boolean
+  ) {
+    socket.emit("startgame", filepath, fennotation, runsetup);
+    setBoardIndex(0);
+    setHasBeenSubmitted(true);
+    const fenList = fennotation.split(" ");
+    setHalfMoves([parseInt(fenList[4])]);
+    setMoves([]);
+    try {
+      setPositions([new Position(fennotation)]);
+    } catch (error) {
+      setPositions([new Position()]);
     }
+  }
 
-    function copyFenToClipboard() {
-        const fen = createFen(boardIndex);
-        navigator.clipboard
-            .writeText(fen)
-            .then(() => {
-                console.log("Copied FEN to clipboard:", fen);
-            })
-            .catch((err) => {
-                console.error("Error copying FEN notation to clipboard:", err);
-            });
+  function copyFenToClipboard() {
+    const fen = createFen(boardIndex);
+    navigator.clipboard
+      .writeText(fen)
+      .then(() => {
+        console.log("Copied FEN to clipboard:", fen);
+      })
+      .catch((err) => {
+        console.error("Error copying FEN notation to clipboard:", err);
+      });
+  }
+
+  function addPosition(position: Position, move: MoveDescriptor) {
+    setPositions((prevState) => {
+      const sliced = prevState.slice(0, boardIndex + 1);
+      return sliced.concat(position);
+    });
+    setBoardIndex(boardIndex + 1);
+    updateHalfMoveCounter(move);
+    setMoves((prevMoves) => {
+      const slicedMoves = prevMoves.slice(0, boardIndex);
+      return slicedMoves.concat(move);
+    });
+    console.log(createPGNString(moves, createFen(0), fullMoves(boardIndex)));
+  }
+
+  function handlePrevMoveButton() {
+    if (boardIndex > 0) {
+      setBoardIndex(boardIndex - 1);
+      const fen = createFen(boardIndex - 1);
+      sendBoardFen(fen);
     }
+  }
 
-    function addPosition(position: Position, move: MoveDescriptor) {
-        setPositions((prevState) => {
-            const sliced = prevState.slice(0, boardIndex + 1);
-            return sliced.concat(position);
-        });
-        setBoardIndex(boardIndex + 1);
-        updateHalfMoveCounter(move);
-        setMoves((prevMoves) => {
-            const slicedMoves = prevMoves.slice(0, boardIndex);
-            return slicedMoves.concat(move);
-        });
-        console.log(
-            createPGNString(moves, createFen(0), fullMoves(boardIndex))
-        );
+  function handleNextMoveButton() {
+    if (boardIndex < positions.length - 1) {
+      setBoardIndex(boardIndex + 1);
+      const fen = createFen(boardIndex + 1);
+      sendBoardFen(fen);
     }
+  }
 
-    function handlePrevMoveButton() {
-        if (boardIndex > 0) {
-            setBoardIndex(boardIndex - 1);
-            const fen = createFen(boardIndex - 1);
-            sendBoardFen(fen);
-        }
+  function createFen(index: number) {
+    const fen = positions[index].fen({
+      fiftyMoveClock: halfMoves[index - 1],
+      fullMoveNumber: fullMoves(index),
+    });
+    return fen;
+  }
+
+  function updateHalfMoveCounter(move: MoveDescriptor) {
+    if (move.movingPiece() === "p" || move.isCapture()) {
+      halfMoves[boardIndex + 1] = 0;
+    } else {
+      halfMoves[boardIndex + 1] = halfMoves[boardIndex] + 1;
     }
+  }
 
-    function handleNextMoveButton() {
-        if (boardIndex < positions.length - 1) {
-            setBoardIndex(boardIndex + 1);
-            const fen = createFen(boardIndex + 1);
-            sendBoardFen(fen);
-        }
-    }
+  function exportPgn() {
+    const pgn = createPGNString(moves, createFen(0), fullMoves(boardIndex));
+    navigator.clipboard
+      .writeText(pgn)
+      .then(() => {
+        console.log("Copied PGN to clipboard:", pgn);
+      })
+      .catch((err) => {
+        console.error("Error copying PGN notation to clipboard:", err);
+      });
+  }
 
-    function createFen(index: number) {
-        const fen = positions[index].fen({
-            fiftyMoveClock: halfMoves[index - 1],
-            fullMoveNumber: fullMoves(index),
-        });
-        return fen;
-    }
+  const isGameOver =
+    positions[boardIndex].isCheckmate() === true ||
+    positions[boardIndex].isStalemate() === true ||
+    positions[boardIndex].isDead() === true ||
+    halfMoves[boardIndex] >= 100;
 
-    function updateHalfMoveCounter(move: MoveDescriptor) {
-        if (move.movingPiece() === "p" || move.isCapture()) {
-            halfMoves[boardIndex + 1] = 0;
-        } else {
-            halfMoves[boardIndex + 1] = halfMoves[boardIndex] + 1;
-        }
-    }
+  return (
+    <div>
+      {hasBeenSubmitted && (
+        <>
+          <MyChessboard
+            pos={positions[boardIndex]}
+            addPosition={addPosition}
+            active={!isGameOver}
+          />
 
-    const isGameOver =
-        positions[boardIndex].isCheckmate() === true ||
-        positions[boardIndex].isStalemate() === true ||
-        positions[boardIndex].isDead() === true ||
-        halfMoves[boardIndex] >= 100;
+          <div>
+            <button
+              onClick={handlePrevMoveButton}
+              data-testid="prev-move-button"
+            >
+              {"<"}
+            </button>
+            <button
+              onClick={handleNextMoveButton}
+              data-testid="next-move-button"
+            >
+              {">"}
+            </button>
+          </div>
+          <div data-testid="board-index">Turn {boardIndex}</div>
+        </>
+      )}
 
-    return (
-        <div>
-            {hasBeenSubmitted && (
-                <>
-                    <MyChessboard
-                        pos={positions[boardIndex]}
-                        addPosition={addPosition}
-						active={!isGameOver}
-                    />
-
-                    <div>
-                        <button
-                            onClick={handlePrevMoveButton}
-                            data-testid="prev-move-button"
-                        >
-                            {"<"}
-                        </button>
-                        <button
-                            onClick={handleNextMoveButton}
-                            data-testid="next-move-button"
-                        >
-                            {">"}
-                        </button>
-                    </div>
-                    <div data-testid="board-index">Turn {boardIndex}</div>
-                </>
-            )}
-
-            {isGameOver && <div>GAME OVER</div>}
-            <Logs />
-            <button onClick={copyFenToClipboard}>Copy current FEN</button>
-            <AIForm
-                handleSubmit={handleSubmit}
-                showFen={true}
-                formId="formChess"
-            />
-        </div>
-    );
+      {isGameOver && <div>GAME OVER</div>}
+      <Logs />
+      <button onClick={copyFenToClipboard}>Copy current FEN</button>
+      <button onClick={exportPgn}>Copy current PGN</button>
+      <AIForm handleSubmit={handleSubmit} showFen={true} formId="formChess" />
+    </div>
+  );
 }
