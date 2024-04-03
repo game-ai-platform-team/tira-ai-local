e4f31202388466c7815bfd5c08febc39f4ba6201
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-04-03 12:02:22 +0300

    chore: Hide play button when nothing submitted

--- app/src/renderer/components/cfour/MyConnectFour.tsx
+++ app/src/renderer/components/cfour/MyConnectFour.tsx
@@ -1,25 +1,27 @@
 import { CFourUIPlayable } from "connect-four-board";
+import { PlayButton } from "../PlayButton";
 
 export function MyConnectFour(props: {
-    moves: number[];
-    boardIndex: number;
-    onMovePlayed;
-    active: boolean;
+  moves: number[];
+  boardIndex: number;
+  onMovePlayed;
+  active: boolean;
 }) {
-    function playMove(move: number) {
-        props.onMovePlayed(move);
-    }
+  function playMove(move: number) {
+    props.onMovePlayed(move);
+  }
 
-    return (
-        <div>
-            <CFourUIPlayable
-                rows={6}
-                columns={7}
-                gameMoves={props.moves}
-                playMove={playMove}
-                move_index={props.boardIndex}
-                active={props.active}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <CFourUIPlayable
+        rows={6}
+        columns={7}
+        gameMoves={props.moves}
+        playMove={playMove}
+        move_index={props.boardIndex}
+        active={props.active}
+      />
+      <PlayButton />
+    </div>
+  );
 }
