0b2bffd4b11f29ddf90d78211ac4407250817dee
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-04-05 12:06:47 +0300

    test(#7): Play button tests
    Co-authored-by: Hipsterisiili <rasmus.marttila@hotmail.com>
    Co-authored-by: Joel Weinitschke <77693693+skeltal2@users.noreply.github.com>

---
+++ app/src/tests/components/PlayButton.test.tsx
@@ -0,0 +1,26 @@
+import { test, describe, expect, vi } from "vitest";
+import { render, fireEvent } from "@testing-library/react";
+import { JSDOM } from "jsdom";
+
+const { window } = new JSDOM("<!doctype html><html><body></body></html>");
+global.window = window;
+global.document = window.document;
+
+import { PlayButton } from "../../renderer/components/PlayButton";
+
+describe("Playbutton", () => {
+  test("Initial state of autoSendMove is false", () => {
+    const { getByLabelText } = render(<PlayButton />);
+    const checkbox = getByLabelText("Send PLAY after move") as HTMLInputElement;
+    expect(checkbox.checked).toBe(true);
+  });
+  test("Clicking on checkbox toggles autoSendMove state", () => {
+    const { getByLabelText } = render(<PlayButton />);
+    const checkbox = getByLabelText("Send PLAY after move") as HTMLInputElement;
+
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toBe(false);
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toBe(true);
+  });
+});
